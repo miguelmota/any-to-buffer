28fe5c4c29ccb5ae4b38d3d77b4e61bd55088919
Miguel Mota <hello@miguelmota.com>
2019-06-23 13:43:36 -0700

    add test

--- example/example.js
+++ example/example.js
@@ -5,4 +5,5 @@ console.log(toBuffer(256)) // <Buffer 01 00>
 console.log(toBuffer(256, { size: 3 })) // <Buffer 00 01 00>
 console.log(toBuffer('0x010203')) // <Buffer 01 02 03>
 console.log(toBuffer('abc')) // <Buffer 61 62 63>
+console.log(toBuffer(Buffer.from('abc'))) // <Buffer 61 62 63>
 console.log(toBuffer(new BN(256))) // <Buffer 01 00>
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const { randomBytes } = require('crypto')
 const toBuffer = require('../')
 
 test('anyToBuffer', t => {
-  t.plan(9)
+  t.plan(10)
 
   t.equal(Buffer.compare(toBuffer(), Buffer.alloc(0)), 0)
   t.equal(Buffer.compare(toBuffer(null), Buffer.alloc(0)), 0)
@@ -14,6 +14,7 @@ test('anyToBuffer', t => {
   t.equal(Buffer.compare(toBuffer('0x010203'), Buffer.from([0x01, 0x02, 0x03])), 0)
   t.equal(Buffer.compare(toBuffer('abc'), Buffer.from([0x61, 0x62, 0x63])), 0)
   t.equal(Buffer.compare(toBuffer(new BN(256)), Buffer.from([0x01, 0x00])), 0)
+  t.equal(Buffer.compare(toBuffer(Buffer.from('abc')), Buffer.from('abc')), 0)
 
   const bytes = randomBytes(10)
   t.equal(Buffer.compare(toBuffer(bytes), bytes), 0)
